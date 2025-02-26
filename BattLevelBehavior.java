49c8b302c234380c0bdd631de9c2d9df2aec1df7
Dev Sehgal <83958063+DevSTheDeveloper@users.noreply.github.com>
2025-02-26 15:49:02 +0000

    Add files via upload

---
+++ BattLevelBehavior.java
@@ -0,0 +1,39 @@
+package devPackage;
+
+import lejos.hardware.Battery;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.Sound;
+import lejos.robotics.subsumption.Behavior;
+
+public class BattLevelBehavior implements Behavior {
+    private boolean suppressed = false;  
+    private static final float LOW_BATTERY_THRESHOLD = 6.5f;  
+
+    @Override
+    public boolean takeControl() {
+        return Battery.getVoltage() < LOW_BATTERY_THRESHOLD; //return true, initiate take control behaviour 
+    }
+
+    @Override
+    public void action() {
+        suppressed = false;
+        while (suppressed == false) {
+            LCD.clear();
+            LCD.drawString("WARNING: Low Battery", 0, 4); //low batt warning
+            Sound.beep(); 
+
+            try {
+                Thread.sleep(800);  //wait for 0.8 seconds before every voltage check
+            } 
+            
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void suppress() {
+        suppressed = true;  //higher priority process takes over with this call
+    }
+}
